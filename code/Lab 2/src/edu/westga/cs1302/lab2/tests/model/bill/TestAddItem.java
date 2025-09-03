7a5c35a23a91db11497e0ad292b3481dc4ab4fb6
eolah1 <eolah1@my.westga.edu>
2025-09-03 16:12:34 -0400

    Added Junit tests for addItem() Tested when item was null and when item was a valid value

--- code/Lab 2/src/edu/westga/cs1302/lab2/tests/model/bill/TestAddItem.java
+++ code/Lab 2/src/edu/westga/cs1302/lab2/tests/model/bill/TestAddItem.java
@@ -4,11 +4,30 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
+import edu.westga.cs1302.lab2.model.Bill;
+import edu.westga.cs1302.lab2.model.BillItem;
+
 class TestAddItem {
 
 	@Test
-	void testWhenNotNull() {
-		Bill item = Bill
+	public void testVailiditem() {
+		Bill bill = new Bill();
+		BillItem item = new BillItem("Pencil", 2.0);
+		
+		bill.addItem(item);
+		
+		assertTrue(bill.getItems().contains(item));
+	}
+	
+	@Test
+	public void testNullItem() {
+		Bill bill = new Bill();
+		
+		Exception exception = assertThrows(IllegalArgumentException.class, () -> {
+			bill.addItem(null);
+		});
+		
+		assertEquals("item must not be null.", exception.getMessage());
 	}
 
 }
