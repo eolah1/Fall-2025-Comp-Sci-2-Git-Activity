7af2c2008d23786ec05060caac17385b18a72a08
eolah1 <eolah1@my.westga.edu>
2025-09-03 15:28:31 -0400

    Add tests for constructor Wrote Junit test for BillItem constructor. Covers edge cases and when name is null.

---
+++ code/Lab 2/src/edu/westga/cs1302/lab2/tests/model/bill_item/TestConstructor.java
@@ -0,0 +1,54 @@
+package edu.westga.cs1302.lab2.tests.model.bill_item;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import edu.westga.cs1302.lab2.model.BillItem;
+
+class TestConstructor {
+
+	@Test
+	public void testWhenNameIsNull() {
+		String name = null;
+		double amount = 12.0;
+		
+		assertThrows(IllegalArgumentException.class, () -> {
+			new BillItem(name, amount);
+		});
+	}
+	
+	@Test
+	public void testValidInputs() {
+		String name = "Bread";
+		double amount = 10.0;
+		
+		BillItem item = new BillItem(name, amount);
+		
+		assertEquals(name, item.getName(), "checking item name");
+		assertEquals(amount, item.getAmount(), "checking amount value");
+	}
+	
+	@Test
+	public void testLowerBoundry() {
+		String name = "Water";
+		double amount = 0.0;
+		
+		assertThrows(IllegalArgumentException.class, () -> {
+			new BillItem(name, amount);
+		});
+	}
+	
+	@Test
+	public void testUpperBoundry() {
+		String name = "Cheese";
+		double amount = 1.0;
+		
+		BillItem item = new BillItem(name, amount);
+		
+		assertEquals(name, item.getName(), "checking item name");
+		assertEquals(amount, item.getAmount(), "checking amount value");
+	}
+	
+
+}
