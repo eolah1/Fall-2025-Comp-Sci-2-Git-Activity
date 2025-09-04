f52fca0f9cf0d9522ed3f4616ff0a2fb0de67f4f
eolah1 <eolah1@my.westga.edu>
2025-09-04 10:47:44 -0400

    Setting up JavaFX Lab3
    Added bill model classes to javafx sample project
    Did not implement test cases

---
+++ code/Lab 2/src/edu/westga/cs1302/lab3/test/model/bill/TestAddItem.java
@@ -0,0 +1,34 @@
+package edu.westga.cs1302.lab3.test.model.bill;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+import edu.westga.cs1302.lab3.model.Bill;
+import edu.westga.cs1302.lab3.model.BillItem;
+
+class TestAddItem {
+
+	@Test
+	public void testVailiditem() {
+		Bill bill = new Bill();
+		BillItem item = new BillItem("Pencil", 2.0);
+		
+		bill.addItem(item);
+		
+		assertTrue(bill.getItems().contains(item));
+	}
+	
+	@Test
+	public void testNullItem() {
+		Bill bill = new Bill();
+		
+		Exception exception = assertThrows(IllegalArgumentException.class, () -> {
+			bill.addItem(null);
+		});
+		
+		assertEquals("item must not be null.", exception.getMessage());
+	}
+}
---
+++ code/Lab 2/src/edu/westga/cs1302/lab3/test/view/bill_view/TestGetText.java
@@ -0,0 +1,53 @@
+package edu.westga.cs1302.lab3.test.view.bill_view;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+import edu.westga.cs1302.lab3.model.Bill;
+import edu.westga.cs1302.lab3.model.BillItem;
+import edu.westga.cs1302.lab3.view.BillView;
+
+class TestGetText {
+
+	@Test
+	public void testEmptyBill() {
+		Bill bill = new Bill();
+		BillView view = new BillView();
+		
+		String expectation = "ITEMS" + System.lineSeparator()
+			+ System.lineSeparator()
+			+ "SUBTOTAL - $0.0" + System.lineSeparator()
+			+ "TAX - $0.0" + System.lineSeparator()
+			+ "TIP - $0.0" + System.lineSeparator()
+			+ "TOTAL - $0.0";
+		
+		assertEquals(expectation, view.getText(bill));
+	}
+	
+	@Test
+	public void testWhenMultipleItems() {
+		Bill bill = new Bill();
+        bill.addItem(new BillItem("Burger", 5.00));
+        bill.addItem(new BillItem("Fries", 2.00));
+        
+        BillView view = new BillView();
+        
+        double subtotal = 7.00;
+        double tax = subtotal * Bill.TAX_RATE;
+        double tip = subtotal * Bill.TIP_RATE;
+        double total = subtotal + tax + tip;
+        
+        String expectation = "ITEMS" + System.lineSeparator()
+        		+ "Burger - 5.0" + System.lineSeparator()
+        		+ "Fries - 2.0" + System.lineSeparator()
+        		+ System.lineSeparator()
+        		+ "SUBTOTAL - $" + subtotal + System.lineSeparator()
+        		+ "TAX - $" + tax + System.lineSeparator()
+        		+ "TIP - $" + tip + System.lineSeparator()
+        		+ "TOTAL - $" + total;
+        
+        assertEquals(expectation, view.getText(bill));
+	}
+
+}
