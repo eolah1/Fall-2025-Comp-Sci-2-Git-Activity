90661e24ec4ae82be7787c2ad3dfc602b25c5419
eolah1 <eolah1@my.westga.edu>
2025-09-04 14:21:47 -0400

    Applied CheckStyle fixes
    Applied a JavaDoc comment for addItem()

--- code/JavaFX Sample Starter/src/main/java/edu/westga/cs1302/lab3/views/MainWindow.java
+++ code/JavaFX Sample Starter/src/main/java/edu/westga/cs1302/lab3/views/MainWindow.java
@@ -14,36 +14,44 @@ import javafx.scene.control.TextField;
  * @version Fall 2025
  */
 public class MainWindow {
-	
+
 	@FXML
-    private TextField amountBox;
+	private TextField amountBox;
 
-    @FXML
-    private TextField nameBox;
+	@FXML
+	private TextField nameBox;
 
-    @FXML
-    private TextArea output;
-    
-    private Bill bill = new Bill();
+	@FXML
+	private TextArea output;
 
-    @FXML
-    public void addItem(ActionEvent event) {
-    	String nameValue = this.nameBox.getText();
-    	double amountValue = Double.parseDouble(this.amountBox.getText());
-    	
-    	BillItem item = new BillItem(nameValue, amountValue);
-    	
-    	this.bill.addItem(item);
-    	
-    	BillView billView = new BillView();
-    	String billText = billView.getText(this.bill);
-    	this.output.setText(billText);
-    }
-    
-    /**
-     * Perform any needed initialization of UI components and underlying objects.
-     */
-    public void initialize() {
+	private Bill bill = new Bill();
 
-    }
+	/**
+	 * Handles the action of adding an item to the bill. This method retrieves the
+	 * item name and amount from the input fields, creates a new BillItem, adds it
+	 * to the bill, and updates the output display.
+	 * 
+	 * @param event The action event that triggered this method (e.g., button
+	 *              click).
+	 */
+	@FXML
+	public void addItem(ActionEvent event) {
+		String nameValue = this.nameBox.getText();
+		double amountValue = Double.parseDouble(this.amountBox.getText());
+
+		BillItem item = new BillItem(nameValue, amountValue);
+
+		this.bill.addItem(item);
+
+		BillView billView = new BillView();
+		String billText = billView.getText(this.bill);
+		this.output.setText(billText);
+	}
+
+	/**
+	 * Perform any needed initialization of UI components and underlying objects.
+	 */
+	public void initialize() {
+
+	}
 }
