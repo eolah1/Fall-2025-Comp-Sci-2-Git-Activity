ff961b61667a7a1471962ad8b792ad31486a331c
eolah1 <eolah1@my.westga.edu>
2025-09-23 16:56:41 -0400

    Completed UI for Adding Tasks
    Added method to allow user to select a task and view it
    Enabled adding tasks to ListView

--- code/Project 1/src/main/java/edu/westga/cs1302/project1/views/MainWindow.java
+++ code/Project 1/src/main/java/edu/westga/cs1302/project1/views/MainWindow.java
@@ -6,6 +6,8 @@ import javafx.scene.control.ComboBox;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 import javafx.scene.control.TextArea;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
 
 /**
  * Controller class for drawing various things to our canvas window.
@@ -17,24 +19,59 @@ import javafx.scene.control.TextArea;
 public class MainWindow {
 	
 	@FXML
+    private Button addToList;
+
+    @FXML
     private TextArea taskDescription;
 
     @FXML
-    private ListView<?> taskList;
+    private ListView<String> taskList;
 
     @FXML
     private TextField taskName;
 
     @FXML
-    private ComboBox<?> taskPriority;
-
-    @FXML
-    void addTitle(ActionEvent event) {
-
+    private ComboBox<String> taskPriority;
+    
+    public void initialize() {
+    	this.taskPriority.getItems().add("High");
+    	this.taskPriority.getItems().add("Medium");
+    	this.taskPriority.getItems().add("Low");
     }
 
     @FXML
-    void popUpFavorite(ActionEvent event) {
+    void addTask(ActionEvent event) {
+    	String name = this.taskName.getText();
+        String description = this.taskDescription.getText();
+        String priority = this.taskPriority.getValue();
+        
+        if (name == null || name.isEmpty()) {
+        	Alert alert = new Alert(Alert.AlertType.ERROR);
+        	alert.setContentText("Task name cannot be empty");
+        	alert.showAndWait();
+        	return;
+        }
+        
+        if (priority == null || priority.isEmpty()) {
+        	Alert alert = new Alert(Alert.AlertType.ERROR);
+        	alert.setContentText("Piority must be selected");
+        	alert.showAndWait();
+        	return;
+        }
+        
+        String tasksView = "(" + priority + ") " + name + "- " + description;
+        this.taskList.getItems().add(tasksView);
+    }
 
+    @FXML
+    void showSelectedTask(javafx.scene.input.MouseEvent event) {
+        String selectedTask = this.taskList.getSelectionModel().getSelectedItem();
+
+        if (selectedTask != null) {
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setHeaderText("Selected Task");
+            alert.setContentText(selectedTask);
+            alert.showAndWait();
+        } 
     }
 }
---
+++ code/Project 1/src/test/java/edu/westga/cs1302/project1/test/TestMainWindow.java
@@ -0,0 +1,14 @@
+package edu.westga.cs1302.project1.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TestMainWindow {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
