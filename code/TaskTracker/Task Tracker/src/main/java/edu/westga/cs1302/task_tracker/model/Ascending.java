5c49671a9da8cee868f494f2d9ba3a27c58854a5
eolah1 <eolah1@my.westga.edu>
2025-10-15 11:50:50 -0400

    Implemented a new Ascending Comparator
    Sorts Tasks based on priority (LOW, MEDIUM, HIGH).

---
+++ code/TaskTracker/Task Tracker/src/main/java/edu/westga/cs1302/task_tracker/model/Ascending.java
@@ -0,0 +1,23 @@
+package edu.westga.cs1302.task_tracker.model;
+
+import java.util.Comparator;
+
+/**
+ * Compares tasks and sorts them in ascending order
+ * 
+ * @version CS 1302
+ * @author Fall 2025
+ */
+
+public class Ascending implements Comparator<Task> {
+
+	@Override
+	public int compare(Task t1, Task t2) {
+		return t1.getPriority().compareTo(t2.getPriority());
+	}
+	
+	@Override 
+	public String toString() {
+		return "Ascending";
+	}
+}
