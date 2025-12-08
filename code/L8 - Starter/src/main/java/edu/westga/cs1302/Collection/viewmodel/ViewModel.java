3c263a4772b2c4d7cb642dc0503549288e04af13
eolah1 <eolah1@my.westga.edu>
2025-12-08 16:33:03 -0500

    Binding View and ViewModel for Storing Collections
    Implemented databinding and event handlers.

--- code/L8 - Starter/src/main/java/edu/westga/cs1302/Collection/viewmodel/ViewModel.java
+++ code/L8 - Starter/src/main/java/edu/westga/cs1302/Collection/viewmodel/ViewModel.java
@@ -9,51 +9,77 @@ import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
 import javafx.collections.FXCollections;
 
-/** Manages utilizing the model and makes properties available to bind the UI elements.
+/**
+ * Provides properties and methods to manage collections
  * 
  * @author CS 1302
  * @version Fall 2025
  */
 public class ViewModel {
 
-	private StringProperty name;
-	private ListProperty<Collection> displayCollections;
-	private ObjectProperty<Collection> selectedCollection;
-	
-	public ViewModel() {
-		this.name = new SimpleStringProperty("");
-		this.displayCollections = new SimpleListProperty<Collection>(FXCollections.observableArrayList());
-		this.selectedCollection = new SimpleObjectProperty<>();
-	}
-	
-	public StringProperty getName() {
-		return name;
-	}
+    private StringProperty name;
+    private ListProperty<Collection> displayCollections;
+    private ObjectProperty<Collection> selectedCollection;
+    
+    /**
+     * Creates a new ViewModel with empty values.
+     */
+    public ViewModel() {
+        this.name = new SimpleStringProperty("");
+        this.displayCollections = new SimpleListProperty<>(FXCollections.observableArrayList());
+        this.selectedCollection = new SimpleObjectProperty<>();
+    }
+    
+    /**
+     * Gets the property for the collection name.
+     * 
+     * @return the name property
+     */
+    public StringProperty getName() {
+        return name;
+    }
 
-	public ListProperty<Collection> getDisplayCollections() {
-		return displayCollections;
-	}
+    /**
+     * Gets the property for the list of collections.
+     * 
+     * @return the displayCollections property
+     */
+    public ListProperty<Collection> getDisplayCollections() {
+        return displayCollections;
+    }
 
-	public ObjectProperty<Collection> getSelectedCollections() {
-		return selectedCollection;
-	}
+    /**
+     * Gets the property for the selected collection.
+     * 
+     * @return the selectedCollection property
+     */
+    public ObjectProperty<Collection> getSelectedCollections() {
+        return selectedCollection;
+    }
 
-	public void addCollection() {
-		String newName = name.get();
-    	if (!newName.isEmpty() && newName != null) {
-    		Collection item = new Collection(newName);
-    		if (!displayCollections.contains(item)) {
-    			displayCollections.add(item);
-    			name.set("");
-    		}
-    	}
-	}
+    /**
+     * Adds a new collection if the name is valid
+     * and not already in the list.
+     */
+    public void addCollection() {
+        String newName = name.get();
+        if (newName != null && !newName.isEmpty()) {
+            Collection item = new Collection(newName);
+            if (!displayCollections.contains(item)) {
+                displayCollections.add(item);
+                name.set("");
+            }
+        }
+    }
 
-	public void removeCollection() {
-		Collection selected = selectedCollection.get();
-    	if (selected != null) {
-    		displayCollections.remove(selected);
-    	}
-	}
-
+    /**
+     * Removes the currently selected collection
+     * from the list if one is selected.
+     */
+    public void removeCollection() {
+        Collection selected = selectedCollection.get();
+        if (selected != null) {
+            displayCollections.remove(selected);
+        }
+    }
 }
